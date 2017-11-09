07b23e9a4539f4330729491d6501d43dac7b60f8
Jean-Marie Lallouet <jean-ma@users.noreply.github.com>
2017-11-09 20:51:09 +0100

    Gameoflife (#2)
    * an isolated cell should die
    
    * neighbors count function
    
    * give life to cell rule
    
    * implement killing rule
    
    * test keep alive rule
    
    * remove code duplication
    
    * add test for the whole universe with one and two iterations
    
    * improve coding style
    
    * fix test data and refactor unit test
    
    * refactor countNeighbors
    
    * remove dummy test object

--- src/main/scala/GameOfLife.scala
+++ src/main/scala/GameOfLife.scala
@@ -1,3 +1,45 @@
+package gameoflife
+/**
+ * The universe of the Game of Life is an infinite two-dimensional orthogonal grid of square cells, each of which is in
+ * one of two possible states, alive or dead, or "populated" or "unpopulated". Every cell interacts with its eight
+ * neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the
+ * following transitions occur:
+ *
+ *  - Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+ *  - Any live cell with two or three live neighbours lives on to the next generation.
+ *  - Any live cell with more than three live neighbours dies, as if by overpopulation.
+ *  - Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+ */
+
 object GameOfLife {
 
+  type Universe = Cell => Liveness
+
+  def countNeighbors(universe: Universe, cell: Cell): Int = {
+      for {
+        i <- -1 to 1
+        j <- -1 to 1
+        if i != 0 || j != 0
+      } yield {
+        Cell(cell.x + i, cell.y + j)
+      }
+    } count {
+      cell => universe(cell) == Alive
+    }
+
+  def next(u: Universe): Universe =
+    (cell: Cell) => countNeighbors(u, cell) match {
+      case n if n < 2 => Dead
+      case n if n == 2 => u(cell)
+      case n if n == 3 => Alive
+      case n if n > 3 => Dead
+      case _ => Dead
+    }
+
 }
+
+case class Cell(x: Int, y: Int)
+
+sealed trait Liveness
+object Alive extends Liveness
+object Dead extends Liveness
---
+++ src/test/scala/GameOfLifeSpec.scala
@@ -0,0 +1,109 @@
+
+import gameoflife.GameOfLife.Universe
+import gameoflife.{GameOfLife, Alive => ♡, Dead => ☠, Cell}
+
+import org.scalatest.FlatSpec
+
+import scala.util.Try
+
+class GameOfLifeSpec extends FlatSpec {
+
+  "An isolated cell" should "die" in {
+    assert(GameOfLife.next(universe)(isolatedCell) === ☠)
+  }
+
+  "Neighbors" should "return the number of alive neighbors" in {
+    assert(GameOfLife.countNeighbors(universe, isolatedCell) === 1)
+  }
+
+  "universe" should "give life to a dead cell with 3 Alive neighbors" in {
+    assert(GameOfLife.next(universe)(deadToAliveCell) === ♡)
+  }
+
+  "universe" should "kill a living cell with 4 or more neighbors" in {
+    assert(GameOfLife.next(universe)(aliveToDeadCell) === ☠)
+  }
+
+  "universe" should "keep alive a cell with 2 neighbors" in {
+    assert(GameOfLife.next(universe)(keepAliveCell) === ♡)
+  }
+
+  "universe" should "give expected universe"in {
+    val actual = GameOfLife.next(universe)
+
+    assertEqual(expectedUniverse, actual, 5)
+  }
+
+  "universe" should "give expected universe of second generation"in {
+    val actual = GameOfLife.next(GameOfLife.next(universe))
+
+    assertEqual(expectedUniverse2Gen, actual, 5)
+  }
+
+  val universe: Universe =
+    cell => {
+      val space = Vector(
+        Vector(♡, ♡, ☠, ☠, ☠),
+        Vector(♡, ♡, ☠, ♡, ☠),
+        Vector(♡, ☠, ☠, ☠, ♡),
+        Vector(☠, ☠, ☠, ♡, ☠)
+      )
+
+      Try {
+        space(cell.x)(cell.y)
+      } getOrElse ☠
+    }
+
+  val expectedUniverse: Universe = {
+    cell => {
+      val space = Vector(
+        Vector(♡, ♡, ♡, ☠, ☠),
+        Vector(☠, ☠, ♡, ☠, ☠),
+        Vector(♡, ♡, ♡, ♡, ♡),
+        Vector(☠, ☠, ☠, ☠, ☠)
+      )
+
+      Try {
+        space(cell.x)(cell.y)
+      } getOrElse ☠
+    }
+  }
+
+  val expectedUniverse2Gen: Universe = {
+    cell => {
+      val space = Vector(
+        Vector(♡, ♡, ♡, ☠, ☠),
+        Vector(☠, ☠, ☠, ☠, ☠),
+        Vector(♡, ♡, ♡, ♡, ☠),
+        Vector(☠, ♡, ♡, ♡, ☠)
+      )
+
+      Try {
+        space(cell.x)(cell.y)
+      } getOrElse ☠
+    }
+  }
+
+  val isolatedCell = Cell(1, 3)
+
+  val borderCell = Cell(2, 4)
+
+  val deadToAliveCell = Cell(2, 3)
+
+  val aliveToDeadCell = Cell(1, 1)
+
+  val keepAliveCell = Cell(2, 0)
+
+  private def assertEqual(expected: Universe, actual: Universe, size: Int) = {
+    for {
+      i <- 0 to size - 1
+      j <- 0 to size - 1
+    } yield {
+      val cell = Cell(i, j)
+      assert(
+        expected(cell) === actual(cell),
+        s"failed on $cell"
+      )
+    }
+  }
+}
