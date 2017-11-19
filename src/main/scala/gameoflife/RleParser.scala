6576d9a16e8fdcb12966dcaa484aa0732a568f9f
Jean-Marie Lallouet <jean.lallouet@deliveryhero.com>
2017-11-19 13:10:45 +0100

    add size reading

--- src/main/scala/gameoflife/RleParser.scala
+++ src/main/scala/gameoflife/RleParser.scala
@@ -3,28 +3,51 @@ package gameoflife
 import scala.util.parsing.combinator.RegexParsers
 
 object RleParser extends RegexParsers {
+  case class Size(x: Int, y: Int)
+
+  type FixedSizeArray = Size => List[List[Liveness]]
+  type FixedSizeLine = Size => List[Liveness]
+
   def apply(input: String): Vector[Vector[Liveness]] = parseAll(rleContent, input) match {
     case Success(result, _) => result
     case failure: NoSuccess => scala.sys.error(failure.msg)
   }
 
-  private def rleContent: Parser[Vector[Vector[Liveness]]] = (comments ~ grid) ^^ {
-    case _ ~ grid => grid
+  def complete(lines: List[List[Liveness]], size: Size): List[List[Liveness]] = {
+    lines.map {
+      line => line ::: List.fill(size.x - line.size)(Dead)
+    } ::: {
+      List.fill(size.y - lines.size)(List.fill(size.x)(Dead))
+    }
   }
+
+  private def rleContent: Parser[Vector[Vector[Liveness]]] = (comments ~ size ~ grid) ^^ {
+    case _ ~ size ~ grid => complete(grid, size).map(_.toVector).toVector
+  }
+
+  private def size: Parser[Size] = " *x *= *".r ~ number ~ ", *y *= *".r ~ number ^^ {
+    case _ ~ x ~ _ ~ y => Size(x, y)
+  }
+
+  private def number: Parser[Int] = """\d+""".r ^^ { token => token.toInt }
 
   private def comments: Parser[_] = rep("#.*".r)
 
-  private def grid: Parser[Vector[Vector[Liveness]]] = rep(line ~ newLines) ^^ {
+  private def grid: Parser[List[List[Liveness]]] = rep(line ~ newLines) ^^ {
     lines => lines.flatMap {
       case line ~ newLines => line +: newLines
-    }.toVector
+    }
   }
 
-  private def line: Parser[Vector[Liveness]] = rep(factorState) ^^ { line => line.toVector.flatten }
+  private def completeWithCells(line: List[Liveness], size: Size): List[Liveness] = {
+    line ::: List.fill(size.x - line.size)(Dead)
+  }
 
-  private def newLines: Parser[Vector[Vector[Liveness]]] = factor ~ "[$!]".r ^^ {
-    case factor ~ _ if factor == 1 => Vector.empty
-    case factor ~ _ => Vector.fill(factor - 1)(Vector.empty)
+  private def line: Parser[List[Liveness]] = rep(factorState) ^^ { line => line.flatten }
+
+  private def newLines: Parser[List[List[Liveness]]] = factor ~ "[$!]".r ^^ {
+    case factor ~ _ if factor == 1 => List.empty
+    case factor ~ _ => List.fill(factor - 1)(List.empty)
   }
 
   private def factorState: Parser[Vector[Liveness]] = factor ~ state ^^ {
--- src/test/scala/gameoflife/RleParserSpec.scala
+++ src/test/scala/gameoflife/RleParserSpec.scala
@@ -3,89 +3,42 @@ package gameoflife
 import org.scalatest.FlatSpec
 
 class RleParserSpec extends FlatSpec {
-  "parser" should "parse single element" in {
-    val expected = Vector(Vector(Alive))
-    val inputGrid = "o!"
-
-    assert(RleParser(inputGrid) === expected)
-  }
-
-  "parser" should "parse Dead element with factor" in {
-    val expected = Vector(Vector(Alive, Alive))
-    val inputGrid = "2o!"
-
-    assert(RleParser(inputGrid) === expected)
-  }
-
-  "parser" should "parse dead and alive elements with factor" in {
-    val expected = Vector(Vector(Dead, Dead, Alive))
-    val inputGrid = "2bo!"
-
-    assert(RleParser(inputGrid) === expected)
-  }
-
-  "parser" should "parse 3 lines" in {
-    val expected = Vector(
-      Vector(Dead, Dead, Alive),
-      Vector(Alive, Dead, Alive),
-      Vector(Alive, Dead, Dead, Alive, Dead))
-    val inputGrid = "2bo$obo$o2bob!"
-
-    assert(RleParser(inputGrid) === expected)
-  }
-
-  "parser" should "parse empty lines" in {
-    val expected = Vector(
-      Vector(Dead, Dead, Alive),
-      Vector.empty[Liveness],
-      Vector(Alive, Dead, Dead, Alive, Dead))
-    val inputGrid = "2bo$$o2bob!"
-
-    assert(RleParser(inputGrid) === expected)
-  }
-
-  "parser" should "parse with new line" in {
-    val expected = Vector(
-      Vector(Dead, Dead, Alive),
-      Vector.empty[Liveness],
-      Vector(Alive, Dead, Dead, Alive, Dead))
-    val inputGrid =
-      """
-        |2bo$$
-        |o2bob!
-        |""".stripMargin
-
-    assert(RleParser(inputGrid) === expected)
-  }
 
   "parser" should "parse with new line with factor" in {
     val expected = Vector(
-      Vector(Dead, Dead, Alive),
-      Vector.empty[Liveness],
-      Vector.empty[Liveness],
-      Vector(Alive, Dead, Dead, Alive, Dead))
+      Vector(Dead, Dead, Alive, Dead, Dead),
+      Vector.fill(5)(Dead),
+      Vector.fill(5)(Dead),
+      Vector(Alive, Dead, Dead, Alive, Dead),
+      Vector.fill(5)(Dead))
+
     val inputGrid =
       """
+        |x = 5, y = 5
         |2bo3$
         |o2bob!
         |""".stripMargin
 
-    assert(RleParser(inputGrid) === expected)
+    val actual = RleParser(inputGrid)
+
+
+    expected.zip(actual.zipWithIndex).foreach {
+      case (le, (la, indexX)) => le.zip(la.zipWithIndex).foreach {
+        case (a, (e, indexY)) => assert(e === a, s"Error at index ($indexX, $indexY)")
+      }
+    }
   }
 
-  "parser" should "parse a file with comments" in {
-    val expected = Vector(
-      Vector(Dead, Dead, Alive),
-      Vector.empty[Liveness],
-      Vector.empty[Liveness],
-      Vector(Alive, Dead, Dead, Alive, Dead))
+  "parser" should "parse a file with x, y information" in {
     val inputGrid =
       """
         |# here is a comment
+        |x = 10, y = 10
         |2bo3$
         |o2bob!
         |""".stripMargin
 
-    assert(RleParser(inputGrid) === expected)
+    assert(10 === RleParser(inputGrid).size)
+    assert(10 === RleParser(inputGrid).head.size)
   }
 }
